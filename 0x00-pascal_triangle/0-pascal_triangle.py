b9100f8b604ee5dd5b36f697c2ece702b36d3fd0
Amolo Chukwudumebi  Joseph <65375943+Roymaniac@users.noreply.github.com>
2023-07-05 12:56:39 +0000

    conform to pep8 validation standard

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """Pascal Triangle Interview Challenge"""
 
-
 def pascal_triangle(n):
     """
-    returns a list of lists of integers 
-    representing the Pascal’s triangle 
+    returns a list of lists of integers
+    representing the Pascal’s triangle
     """
     if n <= 0:
         return []
