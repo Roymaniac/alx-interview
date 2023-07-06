c83298c2aa8630ae015bd894ec8850638759ebdb
Amolo Chukwudumebi  Joseph <65375943+Roymaniac@users.noreply.github.com>
2023-07-06 15:52:12 +0000

    lockboxes task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,29 +10,44 @@ The first box boxes[0] is unlocked
 Return True if all boxes can be opened, else return False
 '''
 
-from collections import deque
-
 def canUnlockAll(boxes):
-    '''
-    method that determines if all the boxes can be opened.
-    '''
-    # Set to keep track of opened boxes
-    opened_boxes = set()
-    opened_boxes.add(0)  # First box is already opened
+    """ method that verifies unlocking """
+    if type(boxes) is not list or not all(type(box) is list for box in boxes):
+        return False
+    # check if list is empty
+    if len(boxes) == 0:
+        return False
+    # check if only exist one box
+    if len(boxes) == 1:
+        return True
+    # check if first box is empty
+    if not boxes[0] and len(boxes) > 1:
+        return False
+    # dictionary of all boxes, all boxes are lock here
+    unlock = {k: False for k in range(len(boxes))}
+    # unlock first box
+    unlock[0] = True
+    # List of all key's first box
+    keys = [key for key in boxes[0]]
+    # Process of unlock boxes
+    while keys:
+        new_key = []
+        for key in keys:
+            if key in unlock.keys() and unlock[key] is False:
+                new_key += boxes[key]
+                unlock[key] = True
+        # If all boxes unlock return True
+        if all(unlock.values()) and len(unlock) == len(boxes):
+            return True
+        # Change keys for the new keys to check
+        keys = new_key
+    return False
 
-    # Queue for BFS traversal
-    queue = deque()
-    queue.append(0)  # Add the first box to the queue
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))
 
-    # Perform BFS traversal
-    while queue:
-        current_box = queue.popleft()
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))
 
-        # Check all the keys in the current box
-        for key in boxes[current_box]:
-            if key not in opened_boxes:
-                opened_boxes.add(key)
-                queue.append(key)
-
-    # Check if all boxes are opened
-    return len(opened_boxes) == len(boxes)
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))
