749ace0e5b05700c570341558aa211b67250d7a2
Amolo Chukwudumebi  Joseph <65375943+Roymaniac@users.noreply.github.com>
2023-08-26 10:38:46 +0000

    clean up

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -42,12 +42,3 @@ def canUnlockAll(boxes):
         # Change keys for the new keys to check
         keys = new_key
     return False
-
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
